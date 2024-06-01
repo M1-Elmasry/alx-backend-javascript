f0e2f5dbc17dcc2629a606173da5cc9a606c0904
M1-Elmasry <mostafamflemasry@outlook.com>
2024-06-01 05:55:00 +0300

    add task 8

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,22 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new Error('Size must be a number');
+    }
+
+    if (!(location && typeof location === 'string')) {
+      throw new Error('Location must be a string');
+    }
+
+    this._size = size;
+    this._location = location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
+}
