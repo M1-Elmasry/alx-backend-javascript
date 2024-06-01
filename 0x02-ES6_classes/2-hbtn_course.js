462775293fabef0dedb16299ac972dd3634cd4da
M1-Elmasry <mostafamflemasry@outlook.com>
2024-06-01 04:46:36 +0300

    add task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-underscore-dangle */
+
+export default class ClassRooms {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  set name(newName) {
+    if (!(newName && typeof newName === 'string')) {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  set length(newLength) {
+    if (!(newLength && typeof newLength === 'number')) {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = newLength;
+  }
+
+  set students(newStudents) {
+    if (!(newStudents && Array.isArray(newStudents) && newStudents.every((element) => typeof element === 'string'))) {
+      throw new TypeError('Students must be a array of strings');
+    }
+    this._students = newStudents;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+}
