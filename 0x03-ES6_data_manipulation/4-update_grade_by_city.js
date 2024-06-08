07674b9d1fdb19abd2994c2f7362939ffb9a9dad
M1-Elmasry <mostafamflemasry@outlook.com>
2024-06-08 18:07:53 +0300

    optimize task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,16 +1,8 @@
-import getStudentsByLocation from './2-get_students_by_loc';
-
 export default function updateStudentGradeByCity(studentArray, city, gradesArray) {
-  if (!(studentArray instanceof Array)) return [];
-  return getStudentsByLocation(studentArray, city).map((studentObj) => {
-    // edit on a copy of param to satisfy eslint (no-param-reassign)
-    const studentObjCopy = { ...studentObj };
-    for (const gradeObj of gradesArray) {
-      if (gradeObj.studentId === studentObjCopy.id) {
-        studentObjCopy.grade = gradeObj.grade;
-      }
-    }
-    if (studentObjCopy.grade === undefined) studentObjCopy.grade = 'N/A';
-    return studentObjCopy;
-  });
+  return studentArray
+    .filter((studentObj) => studentObj.location === city)
+    .map((studentObj) => {
+      const gradeObj = gradesArray.find((gradeObj) => studentObj.id === gradeObj.studentId);
+      return { ...studentObj, grade: gradeObj ? gradeObj.grade : 'N/A' };
+    });
 }
